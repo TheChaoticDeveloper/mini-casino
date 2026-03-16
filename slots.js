1428087c867d92b826ae8c01acbfc66254396664
Andrew B <andrewbachez@yahoo.com>
2026-03-16 18:29:00 -0400

    Refactor spin function for better readability

--- slots.js
+++ slots.js
@@ -1,24 +1,40 @@
-let money=1000;
+let money = 1000;
 
 function spin(){
 
-let bet=parseInt(document.getElementById("bet").value);
+let bet = parseInt(document.getElementById("bet").value);
 
-let r1=Math.floor(Math.random()*9)+1;
-let r2=Math.floor(Math.random()*9)+1;
-let r3=Math.floor(Math.random()*9)+1;
+let r1=document.getElementById("r1");
+let r2=document.getElementById("r2");
+let r3=document.getElementById("r3");
 
-document.getElementById("reels").innerHTML=r1+" "+r2+" "+r3;
+let spins=0;
+
+let animation=setInterval(function(){
+
+r1.innerHTML=Math.floor(Math.random()*9)+1;
+r2.innerHTML=Math.floor(Math.random()*9)+1;
+r3.innerHTML=Math.floor(Math.random()*9)+1;
+
+spins++;
+
+if(spins>15){
+
+clearInterval(animation);
+
+let n1=parseInt(r1.innerHTML);
+let n2=parseInt(r2.innerHTML);
+let n3=parseInt(r3.innerHTML);
 
 let result="";
 
-if(r1===r2 && r2===r3){
+if(n1===n2 && n2===n3){
 
 money+=bet*5;
 result="🎉 JACKPOT!";
 
 }
-else if(r1===r2 || r2===r3){
+else if(n1===n2 || n2===n3){
 
 money+=bet*2;
 result="Small Win";
@@ -31,14 +47,11 @@ result="Try Again";
 
 }
 
-document.getElementById("result").innerHTML=result;
 document.getElementById("money").innerHTML=money;
+document.getElementById("result").innerHTML=result;
 
 }
 
-function resetSlots(){
-
-document.getElementById("reels").innerHTML="🎲 🎲 🎲";
-document.getElementById("result").innerHTML="";
+},100);
 
 }

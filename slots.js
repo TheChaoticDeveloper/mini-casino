ed38a0b947a7179fda7c1cbd711aee9908b63374
Andrew B <andrewbachez@yahoo.com>
2026-03-15 21:32:52 -0400

    Refactor spin function and update money handling

--- slots.js
+++ slots.js
@@ -1,25 +1,44 @@
+let money=1000;
+
 function spin(){
 
-let r1 = Math.floor(Math.random()*9)+1;
-let r2 = Math.floor(Math.random()*9)+1;
-let r3 = Math.floor(Math.random()*9)+1;
+let bet=parseInt(document.getElementById("bet").value);
 
-document.getElementById("reels").innerHTML =
-r1 + " " + r2 + " " + r3;
+let r1=Math.floor(Math.random()*9)+1;
+let r2=Math.floor(Math.random()*9)+1;
+let r3=Math.floor(Math.random()*9)+1;
 
-let result = "";
+document.getElementById("reels").innerHTML=r1+" "+r2+" "+r3;
+
+let result="";
 
 if(r1===r2 && r2===r3){
-result = "🎉 JACKPOT!";
+
+money+=bet*5;
+result="🎉 JACKPOT!";
+
 }
 else if(r1===r2 || r2===r3){
-result = "Small Win!";
+
+money+=bet*2;
+result="Small Win";
+
 }
 else{
-result = "Try Again";
+
+money-=bet;
+result="Try Again";
+
 }
 
-document.getElementById("result").innerHTML = result;
+document.getElementById("result").innerHTML=result;
+document.getElementById("money").innerHTML=money;
 
 }
 
+function resetSlots(){
+
+document.getElementById("reels").innerHTML="🎲 🎲 🎲";
+document.getElementById("result").innerHTML="";
+
+}

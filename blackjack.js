a4bef416c279fb6fb6362e0d643d3fc19862c3dc
Andrew B <andrewbachez@yahoo.com>
2026-03-15 21:26:14 -0400

    Implement basic blackjack game functionality

--- blackjack.js
+++ blackjack.js
@@ -1,1 +1,78 @@
+let player = [];
+let dealer = [];
+
+function drawCard(){
+return Math.floor(Math.random()*10)+1;
+}
+
+function startGame(){
+
+player = [drawCard(),drawCard()];
+dealer = [drawCard(),drawCard()];
+
+updateDisplay();
+
+}
+
+function hit(){
+
+player.push(drawCard());
+
+if(total(player) > 21){
+document.getElementById("result").innerHTML =
+"Bust! Dealer Wins";
+}
+
+updateDisplay();
+
+}
+
+function stand(){
+
+while(total(dealer) < 17){
+dealer.push(drawCard());
+}
+
+let p = total(player);
+let d = total(dealer);
+
+let result="";
+
+if(d>21 || p>d){
+result="You Win!";
+}
+else if(d>p){
+result="Dealer Wins";
+}
+else{
+result="Draw";
+}
+
+document.getElementById("result").innerHTML=result;
+
+updateDisplay();
+
+}
+
+function total(hand){
+
+let sum = 0;
+
+for(let i=0;i<hand.length;i++){
+sum += hand[i];
+}
+
+return sum;
+
+}
+
+function updateDisplay(){
+
+document.getElementById("player").innerHTML =
+"Player: "+player.join(" ");
+
+document.getElementById("dealer").innerHTML =
+"Dealer: "+dealer.join(" ");
+
+}
 

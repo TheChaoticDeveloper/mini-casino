5bc467236b0c12dcd05e6ae9966a1f4858674214
Andrew B <andrewbachez@yahoo.com>
2026-03-15 21:25:36 -0400

    Implement spin function for slot machine
    Added a spin function to generate random slot results and display outcomes.

--- slots.js
+++ slots.js
@@ -1,1 +1,25 @@
+function spin(){
+
+let r1 = Math.floor(Math.random()*9)+1;
+let r2 = Math.floor(Math.random()*9)+1;
+let r3 = Math.floor(Math.random()*9)+1;
+
+document.getElementById("reels").innerHTML =
+r1 + " " + r2 + " " + r3;
+
+let result = "";
+
+if(r1===r2 && r2===r3){
+result = "🎉 JACKPOT!";
+}
+else if(r1===r2 || r2===r3){
+result = "Small Win!";
+}
+else{
+result = "Try Again";
+}
+
+document.getElementById("result").innerHTML = result;
+
+}
 
